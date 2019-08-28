3ea758db673f145817663f1a04cc1f1ceb4f6311
Dave Williams <53339284+auraria@users.noreply.github.com>
2019-08-28 14:23:28 -0400

    v0.4
    Fixed a bunch of robtex parsing.

--- intel.py
+++ intel.py
@@ -1,10 +1,16 @@
+import json
+import re
+import sys
+import requests
+import urllib3
 from bs4 import BeautifulSoup
-import requests, json, re, sys, urllib3
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 urlhaus = [] 
-robtex = [] 
+robtexip = [] 
+robtexurl = [] 
 threatcrowd = [] 
 lookup = [] #list of lookup responses
 
@@ -20,31 +26,87 @@ class urlgrab:
             threatcrowd.append(self.url1)    
             self.url2 = 'https://www.threatcrowd.org/domain.php?domain={}'.format(i) 
             threatcrowd.append(self.url2)    
-            self.url3 = 'https://www.robtex.com/ip-lookup/{}'.format(i)
-            robtex.append(self.url3)   
+            if re.match(r"[\d\.]+",i) is not None: #This regex searches for IP address format and will return the ip, if not it will go to the else statement on 21
+                self.url = 'https://www.robtex.com/ip-lookup/{}'.format(i) #robtex url to search with the ip from self.ip in the url to search
+                robtexip.append(self.url) #appends robtex url with ips to search to the urls list
+            else:
+                self.url = 'https://www.robtex.com/ip-lookup/{}'.format(i) #robtex url to search with the ip from self.ip in the url to search
+                robtexurl.append(self.url) #appends robtex url with ips to search to the urls list 
+            
 
 
     def get(self):
 
-        for u in robtex:
-            response = requests.get(u, verify=False) 
-            soup = BeautifulSoup(response.text, 'html.parser')  
-            results = soup.find_all('div', class_='dns')    
-            results2 = re.findall(r"[\w\s\-\_\.\,\(\)\!\#\$\%\^\&\*\@\;\:]+(?=\<\/)", str(results), re.M|re.I) 
-            if len(results2) > 40:
-                lookup.append(results2[3]+' | '+results2[0]+results2[1]+' | '+results2[7]+' | '+results2[5]+' | '+results2[9]+' | '+results2[11])
-            elif len(results2) >= 30:
-                lookup.append(results2[3]+' | '+results2[0]+''+results2[1]+' | '+results2[5]+' | '+results2[9]+' | '+results2[13]+' | '+results2[15])
-            #elif len(results2) <= 40:
-            #    lookup.append(results2[0]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8]+' | '+results2[10]+' | '+results2[12]) 
-            elif len(results2) > 11:
-                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8]+' | '+results2[10]+' | '+results2[11])
-            elif len(results2) > 10:
-                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8]+' | '+results2[10])
-            elif len(results2) > 4:
+        for i in robtexip: #loop to go through urls in the urls list
+            response = requests.get(i, verify=False) #get request to completed robtex url from above
+            soup = BeautifulSoup(response.text, 'html.parser')  #use Beautifulsoup's html parser for the output and assign that to soup
+            results = soup.find_all('div', class_='dns')    #take html output from soup, find all div sets, under the class dns(this is all found from the html output itself)
+            results2 = re.findall(r"(?<=\>)[\w\.\-\,\(\)\s\/]+(?!\<\/b)", str(results), re.M|re.I) #regex find all non-html data we want to view
+            if len(results2) >= 13:
+                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[4]+' | '+results2[8]+' | '+results2[12])
+            elif len(results2) == 12:
+                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8]+' | '+results2[10]) 
+            elif len(results2) == 11:
+                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8]+' | '+results2[10]) 
+            elif len(results2) == 10:
+                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8])
+            elif len(results2) == 9:
+                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8])
+            elif len(results2) == 8:
+                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[4]+' | '+results2[6])
+            elif len(results2) == 7:
+                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[4]+' | '+results2[6])
+            elif len(results2) == 6:
+                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[4])
+            elif len(results2) == 5:
                 lookup.append(results2[0]+' | '+results2[2]+' | '+results2[4])
             elif len(results2) == 4:
                 lookup.append(results2[0]+' | '+results2[2]+' | '+results2[3])
+            elif len(results2) == 3:
+                lookup.append(results2[0]+' | '+results2[2])
+            elif len(results2) == 2:
+                lookup.append(results2[0]+' | '+results2[1])
+            elif len(results2) == 1:
+                lookup.append(results2[0])
+            elif len(results2) == 0:
+                lookup.append(i+' Nothing Found! Trying pinging the host and looking up that IP')
+
+        for u in robtexurl: #loop to go through urls in the urls list
+            response = requests.get(u, verify=False) #get request to completed robtex url from above
+            soup = BeautifulSoup(response.text, 'html.parser')  #use Beautifulsoup's html parser for the output and assign that to soup
+            results = soup.find_all('div', class_='dns')    #take html output from soup, find all div sets, under the class dns(this is all found from the html output itself)
+            results22 = re.findall(r"(?<=\>)[\w\.\-\,\(\)\d\s\/]+", str(results), re.M|re.I) #regex find all non-html data we want to view
+            results2 = re.findall(r"[\w\.\-\,\s\.]{3,}",str(results22), re.I|re.M)
+            if len(results2) > 14:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2]+' | '+results2[4]+' | '+results2[8]+' | '+results2[12]+' | '+results2[14]+' | '+results2[15])
+            elif len(results2) == 14:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2]+' | '+results2[4]+' | '+results2[8]+' | '+results2[12])
+            elif len(results2) == 13:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2]+' | '+results2[4]+' | '+results2[8]+' | '+results2[12])
+            elif len(results2) == 12:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8]+' | '+results2[10]) 
+            elif len(results2) == 11:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8]+' | '+results2[10]) 
+            elif len(results2) == 10:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8])
+            elif len(results2) == 9:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8])
+            elif len(results2) == 8:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2]+' | '+results2[4]+' | '+results2[6])
+            elif len(results2) == 7:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2]+' | '+results2[4]+' | '+results2[6])
+            elif len(results2) == 6:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2]+' | '+results2[4])
+            elif len(results2) == 5:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2]+' | '+results2[4])
+            elif len(results2) == 4:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2]+' | '+results2[3])
+            elif len(results2) == 3:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2])
+            elif len(results2) == 2:
+                lookup.append(results2[0]+results2[1])
+            elif len(results2) == 1:
+                lookup.append(results2[0])
             elif len(results2) == 0:
                 lookup.append(u+' Nothing Found! Trying pinging the host and looking up that IP')
         for u in urlhaus: 
