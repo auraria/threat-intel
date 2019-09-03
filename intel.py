8d12a29d577d3f0218e94026d93486da959dd5c1
Dave Williams <53339284+auraria@users.noreply.github.com>
2019-09-03 08:10:06 -0400

    v0.8
    Fixed additional robtex parsing and completely removed certain array items to make parsing easier.

--- intel.py
+++ intel.py
@@ -41,29 +41,89 @@ class urlgrab:
             response = requests.get(i, verify=False) #get request to completed robtex url from above
             soup = BeautifulSoup(response.text, 'html.parser')  #use Beautifulsoup's html parser for the output and assign that to soup
             results = soup.find_all('div', class_='dns')    #take html output from soup, find all div sets, under the class dns(this is all found from the html output itself)
-            results2 = re.findall(r"(?<=\>)[\w\.\-\,\(\)\s\/]+(?!\<\/b)", str(results), re.M|re.I) #regex find all non-html data we want to view
+            results2 = re.findall(r"(?<=\>)[\w\.\-\,\(\)\:\s\/]+(?!\<\/b)", str(results), re.M|re.I) #regex find all non-html data we want to view
+            try:
+                results2.remove('asname')
+            except:
+                pass
+            try:
+                results2.remove('a')
+            except:
+                pass
+            try:
+                results2.remove('ptr')
+            except:
+                pass
+            try:
+                results2.remove('bgp')
+            except:
+                pass
+            try:
+                results2.remove('descr')
+            except:
+                pass
+            try:
+                results2.remove('location')
+            except:
+                pass
+            try:
+                results2.remove('whois')
+            except:
+                pass
+            try:
+                results2.remove('route')
+            except:
+                pass
+            try:
+                results2.remove('rout')
+            except:
+                pass
+            try:
+                results2.remove('whoi')
+            except:
+                pass
+            try:
+                results2.remove('pt')
+            except:
+                pass
+            try:
+                results2.remove('asnam')
+            except:
+                pass
+            try:
+                results2.remove('desc')
+            except:
+                pass
+            try:
+                results2.remove('bg')
+            except:
+                pass
+            try:
+                results2.remove('locatio')
+            except:
+                pass
             if len(results2) >= 13:
-                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[4]+' | '+results2[8]+' | '+results2[12])
+                lookup.append(results2[0]+' | '+results2[1]+' | '+results2[2]+' | '+results2[4]+' | '+results2[8]+' | '+results2[12])
             elif len(results2) == 12:
                 lookup.append(results2[0]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8]+' | '+results2[10]) 
             elif len(results2) == 11:
-                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8]+' | '+results2[10]) 
+                lookup.append(results2[0]+' | '+results2[1]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8]+' | '+results2[10]) 
             elif len(results2) == 10:
-                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8])
+                lookup.append(results2[0]+' | '+results2[1]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8])
             elif len(results2) == 9:
-                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8])
+                lookup.append(results2[0]+' | '+results2[1]+' | '+results2[2]+' | '+results2[6]+' | '+results2[8])
             elif len(results2) == 8:
-                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[4]+' | '+results2[6])
+                lookup.append(results2[0]+' | '+results2[1]+' | '+results2[2]+' | '+results2[4]+' | '+results2[6])
             elif len(results2) == 7:
-                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[4]+' | '+results2[6])
+                lookup.append(results2[0]+' | '+results2[1]+' | '+results2[2]+' | '+results2[4]+' | '+results2[6])
             elif len(results2) == 6:
-                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[4])
+                lookup.append(results2[0]+' | '+results2[1]+' | '+results2[2]+' | '+results2[4])
             elif len(results2) == 5:
-                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[4])
+                lookup.append(results2[0]+' | '+results2[1]+' | '+results2[2]+' | '+results2[4])
             elif len(results2) == 4:
-                lookup.append(results2[0]+' | '+results2[2]+' | '+results2[3])
+                lookup.append(results2[0]+' | '+results2[1]+' | '+results2[2]+' | '+results2[3])
             elif len(results2) == 3:
-                lookup.append(results2[0]+' | '+results2[2])
+                lookup.append(results2[0]+' | '+results2[1]+' | '+results2[2])
             elif len(results2) == 2:
                 lookup.append(results2[0]+' | '+results2[1])
             elif len(results2) == 1:
@@ -75,40 +135,45 @@ class urlgrab:
             response = requests.get(u, verify=False) #get request to completed robtex url from above
             soup = BeautifulSoup(response.text, 'html.parser')  #use Beautifulsoup's html parser for the output and assign that to soup
             results = soup.find_all('div', class_='dns')    #take html output from soup, find all div sets, under the class dns(this is all found from the html output itself)
-            results22 = re.findall(r"(?<=\>)[\w\.\-\,\(\)\d\s\/]+", str(results), re.M|re.I) #regex find all non-html data we want to view
-            results2 = re.findall(r"[\w\.\-\,\s\.]{3,}",str(results22), re.I|re.M)
-            if len(results2) > 14:
-                lookup.append(results2[2]+' | '+results2[0]+results2[1]+' | '+results2[4]+' | '+results2[8]+' | '+results2[12]+' | '+results2[14]+' | '+results2[15])
-            elif len(results2) == 14:
-                lookup.append(results2[2]+' | '+results2[0]+results2[1]+' | '+results2[4]+' | '+results2[8]+' | '+results2[12])
-            elif len(results2) == 13:
-                lookup.append(results2[2]+' | '+results2[0]+results2[1]+' | '+results2[4]+' | '+results2[8]+' | '+results2[12])
-            elif len(results2) == 12:
-                lookup.append(results2[2]+' | '+results2[0]+results2[1]+' | '+results2[6]+' | '+results2[8]+' | '+results2[10]) 
-            elif len(results2) == 11:
-                lookup.append(results2[2]+' | '+results2[0]+results2[1]+' | '+results2[6]+' | '+results2[8]+' | '+results2[10]) 
-            elif len(results2) == 10:
-                lookup.append(results2[2]+' | '+results2[0]+results2[1]+' | '+results2[6]+' | '+results2[8])
-            elif len(results2) == 9:
-                lookup.append(results2[2]+' | '+results2[0]+results2[1]+' | '+results2[6]+' | '+results2[8])
-            elif len(results2) == 8:
-                lookup.append(results2[2]+' | '+results2[0]+results2[1]+' | '+results2[4]+' | '+results2[6])
-            elif len(results2) == 7:
-                lookup.append(results2[2]+' | '+results2[0]+results2[1]+' | '+results2[4]+' | '+results2[6])
-            elif len(results2) == 6:
-                lookup.append(results2[2]+' | '+results2[0]+results2[1]+' | '+results2[4])
-            elif len(results2) == 5:
-                lookup.append(results2[2]+' | '+results2[0]+results2[1]+' | '+results2[4])
-            elif len(results2) == 4:
-                lookup.append(results2[2]+' | '+results2[0]+results2[1]+' | '+results2[3])
-            elif len(results2) == 3:
-                lookup.append(results2[2]+' | '+results2[0]+results2[1])
-            elif len(results2) == 2:
-                lookup.append(results2[0]+results2[1])
-            elif len(results2) == 1:
-                lookup.append(results2[0])
-            elif len(results2) == 0:
+            results22 = re.findall(r"(?<=\>)[\w\.\-\,\(\)\:\d\s\/]+", str(results), re.M|re.I) #regex find all non-html data we want to view
+            results2 = re.findall(r"[\w\.\-\,\:\s\.]{3,}",str(results22), re.I|re.M)
+            try:
+                results2.remove('asname')
+            except:
+                pass
+            try:
+                results2.remove('a')
+            except:
+                pass
+            try:
+                results2.remove('ptr')
+            except:
+                pass
+            try:
+                results2.remove('bgp')
+            except:
+                pass
+            try:
+                results2.remove('descr')
+            except:
+                pass
+            try:
+                results2.remove('location')
+            except:
+                pass
+            try:
+                results2.remove('whois')
+            except:
+                pass
+            try:
+                results2.remove('route')
+            except:
+                pass
+            
+            if len(results2) == 0:
                 lookup.append(u+' Nothing Found! Trying pinging the host and looking up that IP')
+            else:
+                lookup.append(results2[0]+results2[1]+' | '+results2[2]+' | '+results2[5]+' | '+results2[7]+' | '+results2[8]+' '+results2[9])
         for u in urlhaus: 
             response = requests.get(u, verify=False) 
             soup = BeautifulSoup(response.text, 'html.parser') 
